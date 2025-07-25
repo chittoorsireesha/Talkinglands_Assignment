f0d38ce564b90e6577e60693df5025328d187c0a
chittoorsireesha <chittoorsireesha@gmail.com>
2025-07-25 16:27:30 +0530

    main.tf

---
+++ terraform-nginx/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "nginx_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "${var.environment}-nginx"
+  }
+}
+
+resource "aws_security_group" "nginx_sg" {
+  name        = "${var.environment}-nginx-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-nginx-sg"
+  }
+}
