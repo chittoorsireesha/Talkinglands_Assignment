797485c3b724cbd3aded55749542f9a90a2b2b84
chittoorsireesha <chittoorsireesha@gmail.com>
2025-07-25 16:55:07 +0530

    Create app.js

---
+++ Task-2 : nodejs-app/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res) => {
+  res.write("Hello from Node.js App - Deployed via CI/CD!");
+  res.end();
+}).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
