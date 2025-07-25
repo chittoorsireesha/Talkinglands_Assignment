e8ad6dacbfe617c0c0c875cc0a4272745e6f78c4
chittoorsireesha <chittoorsireesha@gmail.com>
2025-07-25 17:30:31 +0530

    Update app.js

--- Task-2 : nodejs-app/app.js
+++ Task-2 : nodejs-app/app.js
@@ -1,9 +1,1 @@
-const http = require('http');
-const port = 3000;
 
-http.createServer((req, res) => {
-  res.write("Hello from Node.js App - Deployed via CI/CD!");
-  res.end();
-}).listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
